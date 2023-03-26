7c61c7ac553b1b6be3c9f1a40abb95d6d311400c
Agostina Ramos <110774783+Agossramos@users.noreply.github.com>
2023-03-26 20:48:18 -0300

    se agregó renuevespast y renuevesupcomming

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,29 +3,28 @@ const contenedorCheck = document.getElementById("checks");
 
 // Function cargar checks
 export function createCheckBoxs(array) {
-    let arrayCountrys = array.map((event) => event.category);
-    let setCountry = new Set(arrayCountrys);
-    let arrayChecks = Array.from(setCountry);
-    let checkboxs = "";
-    arrayChecks.forEach((category) => {
-      checkboxs += `<div class="form-check form-switch">
+  let arrayCountrys = array.map((event) => event.category);
+  let setCountry = new Set(arrayCountrys);
+  let arrayChecks = Array.from(setCountry);
+  let checkboxs = "";
+  arrayChecks.forEach((category) => {
+    checkboxs += `<div class="form-check form-switch">
            <input class="form-check-input" type="checkbox" role="switch" id="${category}" value="${category}">
            <label class="form-check-label" for="${category}">${category}</label>
          </div>`;
-    });
-    contenedorCheck.innerHTML = checkboxs;
-  }
-  
+  });
+  contenedorCheck.innerHTML = checkboxs;
+}
 
 // Function card todos los eventos
 export function addCardsEvents(events) {
-    if (events.length == 0) {
-      contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
-      return;
-    }
-    let cards = "";
-    events.forEach((event) => {
-      cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
+  if (events.length == 0) {
+    contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
+    return;
+  }
+  let cards = "";
+  events.forEach((event) => {
+    cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
                   <img src="${event.image}" class="card-img-top mh-70 object-fit-cover" alt="${event.name}">
                   <div class="card-body text-center">
                     <h2 class="card-title"><strong>${event.name}</strong></h2>
@@ -34,22 +33,20 @@ export function addCardsEvents(events) {
                     <a href="./details.html?id=${event._id}" class="btn btn-primary" id="botoncards">Details</a>
                   </div>
               </div>`;
-    });
-    contenedor.innerHTML = cards;
-  }
-  
- 
+  });
+  contenedor.innerHTML = cards;
+}
 
 // Function eventos pasados
 export function addCardsEventsPast(events, date) {
-    if (events.length == 0) {
-      contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
-      return;
-    }
-    let cards = "";
-    events.forEach((event) => {
-      if (event.date < date) {
-        cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
+  if (events.length == 0) {
+    contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
+    return;
+  }
+  let cards = "";
+  events.forEach((event) => {
+    if (event.date < date) {
+      cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
                     <img src="${event.image}" class="card-img-top mh-70 object-fit-cover;
                     object-fit: cover;" alt="${event.name}">
                     <div class="card-body text-center">
@@ -59,21 +56,21 @@ export function addCardsEventsPast(events, date) {
                       <a href="./details.html?id=${event._id}" class="btn btn-primary" id="botoncards">Details</a>
                     </div>
                   </div>`;
-      }
-    });
-    contenedor.innerHTML = cards;
-  }
+    }
+  });
+  contenedor.innerHTML = cards;
+}
 
 // Function eventos futuros
 export function addCardsEventsUpComming(events, date) {
-    if (events.length == 0) {
-      contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
-      return;
-    }
-    let cards = "";
-    events.forEach((event) => {
-      if (event.date > date) {
-        cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
+  if (events.length == 0) {
+    contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No hay coincidencias</h2>`;
+    return;
+  }
+  let cards = "";
+  events.forEach((event) => {
+    if (event.date > date) {
+      cards += `<div class="card text-bg-light mb-3" style="max-width: 20rem;">
                   <img src="${event.image}" class="card-img-top mh-70 object-fit-cover;
                   object-fit: cover;" alt="${event.name}">
                   <div class="card-body text-center">
@@ -83,32 +80,117 @@ export function addCardsEventsUpComming(events, date) {
                   <a href="./details.html?id=${event._id}" class="btn btn-primary" id="botoncards">Details</a>
               </div>
               </div>`;
-      }
-    });
-    contenedor.innerHTML = cards;
-  }
-//FILTROS
+    }
+  });
+  contenedor.innerHTML = cards;
+}
 
+//FILTROS
 export function filterOfText(events, text) {
-    let arrayFiltrado = events.filter((elemento) =>
-      elemento.name.toLowerCase().includes(text.toLowerCase())
-    );
+  let arrayFiltrado = events.filter((elemento) =>
+    elemento.name.toLowerCase().includes(text.toLowerCase())
+  );
+  return arrayFiltrado;
+}
+
+export function filterOfCategory(array) {
+  let checkboxs = document.querySelectorAll("input[type='checkbox']");
+  let arrayChecks = Array.from(checkboxs);
+  let arrayChecksChecked = arrayChecks.filter((check) => check.checked);
+  let arrayChecksCheckedValues = arrayChecksChecked.map(
+    (checkChecked) => checkChecked.value
+  );
+  let arrayFiltrado = array.filter((elemento) =>
+    arrayChecksCheckedValues.includes(elemento.category)
+  );
+  if (arrayChecksChecked.length > 0) {
     return arrayFiltrado;
   }
-  
-  export function filterOfCategory(array) {
-    let checkboxs = document.querySelectorAll("input[type='checkbox']");
-    let arrayChecks = Array.from(checkboxs);
-    let arrayChecksChecked = arrayChecks.filter((check) => check.checked);
-    let arrayChecksCheckedValues = arrayChecksChecked.map(
-      (checkChecked) => checkChecked.value
-    );
-    let arrayFiltrado = array.filter((elemento) =>
-      arrayChecksCheckedValues.includes(elemento.category)
-    );
-    if (arrayChecksChecked.length > 0) {
-      return arrayFiltrado;
-    }
-    return array;
+  return array;
+}
+
+//Function pagina stats
+
+//saco los valores que van en la primera tabla
+export function primeraTabla(array) {
+  let aMenorAsistencia = array.reduce((prev, current) =>
+    (prev.assistence ? prev.assistence : prev.estimate) > (current.assistence ? current.assistence : current.estimate) ? prev : current).name;
+  let aMayorAsistencia = array.reduce((prev, current) =>
+    (prev.assistence ? prev.assistence : prev.estimate) < (current.assistence ? current.assistence : current.estimate) ? prev : current).name;
+  let mayorCapacidad = array.sort((a, b) => b.capacity - a.capacity)[0].name;
+
+  let resultado = {
+    eventConMenorAsistencia: aMenorAsistencia,
+    eventConMayorAsistencia: aMayorAsistencia,
+    eventoMayorCapacidad: mayorCapacidad,
+  };
+  return resultado;
+}
+
+//lleno cada tabla
+export function llenarTabla(datos, container) {
+  let tr = document.createElement("tr");
+  for (let indice in datos) {
+    // console.log(datos[indice]);
+    let td = document.createElement("td");
+    td.innerText = datos[indice];
+    tr.appendChild(td);
   }
-  
+  container.appendChild(tr);
+}
+
+// saco las categorias
+export function sacarCategorys(array) {
+  return [
+    ...new Set(
+      array
+        .map((item) => item.category)
+        .flat()
+        .map((item) => item.toLowerCase())
+    ),
+  ];
+}
+
+//saco datos segunda tabla
+export function segundaTabla(arrayEvents, arrayGeneros) {
+  let eventsXcategory = arrayGeneros.forEach((category) =>
+    arrayEvents.filter((event) => event.category[0].toLowerCase() == category));
+  revenuesUpComming(eventsXcategory); //falta el parametro del nombre de la categoria );
+  return eventsXcategory;
+}
+
+//saco el porcentaje de asistencia y estimated
+// export function percentage(array){
+//   for (let indice in array){
+//   let arrayPercentage= ((array[indice].assistance / array[indice].capacity) * 100);
+//   return arrayPercentage;
+//   }
+
+//   console.log(arrayPercentage);
+// }
+
+//realizo la revenues del up comming 
+export function revenuesUpComming (array, category){
+  for (let indice in array){
+    if (array[indice] == category){
+      return revenues = ((array[indice].price) * (array[indice].estimate))
+    }
+    console.log(revenues)
+  }
+}
+
+export function terceraTabla(arrayEvents, arrayGeneros) {
+  let eventsXcategory = arrayGeneros.forEach((category) =>
+    arrayEvents.filter((event) => event.category[0].toLowerCase() == category));
+  revenuesPast(eventsXcategory); //falta el parametro del nombre de la categoria );
+  return eventsXcategory;
+}
+
+export function revenuesPast (array, category){
+  for (let indice in array){
+    if (array[indice] == category){
+      return revenues = ((array[indice].price) * (array[indice].assistence))
+    }
+    console.log(revenues)
+  }
+}
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,3 +1,5 @@
+import { primeraTabla, segundaTabla, sacarCategorys, llenarTabla, revenuesUpComming} from "../js/functions.js";
+
 const tbody1 = document.getElementById("tbody1");
 const tbody2 = document.getElementById("tbody2");
 // const tbody3 = document.getElementById("tbody3");
@@ -7,55 +9,20 @@ async function getEvents() {
     .then((response) => response.json())
     .then((data) => {
       let eventsList = data.events;
-      llenarTabla(tablaUno(eventsList), tbody1); //genero la primera parte de la tabla
-      let categorys = sacarCategorys(eventsList); //genero la segunda parte de la tabla
-      llenarTabla(tablaDos(eventsList, categorys), tbody2);
-      console.log(percentage(eventsList));
+      llenarTabla(primeraTabla(eventsList), tbody1); //genero la primera tabla
+      let categorys = sacarCategorys(eventsList); //genero la segunda tabla
+      llenarTabla(segundaTabla(eventsList, categorys), tbody2);
     }).catch((err) => console.error(err));
 }getEvents();
 
-//saco los valores que van en la primera parte de la tabla
-function tablaUno(array){
-    let aMenorAsistencia = array.reduce((prev, current) => ((prev.assistence ? prev.assistence : prev.estimate) > (current.assistence ? current.assistence : current.estimate)) ? prev : current).name
-    let aMayorAsistencia = array.reduce((prev, current) => ((prev.assistence ? prev.assistence : prev.estimate) < (current.assistence ? current.assistence : current.estimate)) ? prev : current).name
-    let mayorCapacidad = array.sort((a,b) => b.capacity - a.capacity)[0].name
-
-    let resultado = {
-        eventConMenorAsistencia: aMenorAsistencia,
-        eventConMayorAsistencia: aMayorAsistencia,
-        eventoMayorCapacidad: mayorCapacidad
-    }
-    return resultado
-}
-
-// saco la categoria
-function sacarCategorys(array) {
-  return [...new Set((array.map(item => item.category).flat()).map(item => item.toLowerCase()))];
-}
-
-function tablaDos(arrayEvents, arrayGeneros) {
-  let eventsXgenero = arrayGeneros.forEach((genero) =>
-    arrayEvents.filter((event) => event.category[0].toLowerCase() == genero));
-  console.log(eventsXgenero);
-}
+async function getEvents2() {
+    await fetch("../assets/data/amazing.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let eventsList = data.events;
+        let categorys = sacarCategorys(eventsList); //genero la segunda tabla
+        llenarTabla(segundaTabla(eventsList, categorys), tbody2);
+      }).catch((err) => console.error(err));
+  }getEvents2();
 
 
-function llenarTabla(datos, container) {
-  let tr = document.createElement("tr");
-  for (let clave in datos) {
-    console.log(datos[clave]);
-    let td = document.createElement("td");
-    td.innerText = datos[clave];
-    tr.appendChild(td);
-  }
-  container.appendChild(tr);
-}
-//saco el porcentaje de asistencia y estimated
-function percentage(array){
-    for (let indice in array){
-    let arrayPercentage= ((array[indice].assistance / array[indice].capacity) * 100);
-    return arrayPercentage;
-    }
-    
-    console.log(arrayPercentage);
-}
